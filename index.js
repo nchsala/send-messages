a2b396a0cb4c5c9fda5b1d669c237dd47149d15b
Ignacio Salaberria <nacho.sr09@gmail.com>
2022-03-21 23:03:47 -0300

    I don't care about credentials right now lol

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { ClientManager } = require("./class/ClientManager");
 (async function(){
     const topic = "TEST.QUEUE.TOP"
     const testMessages = Array.from({ length: 100 }, (_, i) => `Message ${i}`);
-    const clientManager = ClientManager.fromMqLight({ service: 'amqp://localhost', user: "guest", password: "guest" })
+    const clientManager = ClientManager.fromMqLight({ service: 'amqp://localhost'})
     const batchMessageSender = new BatchMessageSender(clientManager);
     await batchMessageSender.send(topic, testMessages);
     return;
