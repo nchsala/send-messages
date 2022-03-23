78d241d7988ab910286edcf73ccf918908cd7eab
Ignacio Salaberria <nacho.sr09@gmail.com>
2022-03-22 21:14:09 -0300

    just sort lines

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const { ClientManager } = require("./class/ClientManager");
 const { MqLightWrapper } = require("./class/MqLightClient");
 
 (async function(){
-    const topic = "TEST.QUEUE.TOP"
-    const testMessages = Array.from({ length: 10000 }, (_, i) => `Message ${i}`);
     const clientManager = ClientManager.create({ service: 'amqp://localhost'})
     const mqLightWrapper = new MqLightWrapper(clientManager);
     const batchMessageSender = new BatchMessageSender(mqLightWrapper);
+    
+    const topic = "TEST.QUEUE.TOP"
+    const testMessages = Array.from({ length: 10000 }, (_, i) => `Message ${i}`);
+    
     await batchMessageSender.send(topic, testMessages);
     console.log("Mensajes enviados");
     process.exit(0);
